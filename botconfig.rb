d08a126583e99e7ce4713bb4b4429b44977061c4
Jason Preston <jason@prestons.me>
2014-07-02 17:51:09 -0700

    added uptime, fixed some bugs

--- botconfig.rb
+++ botconfig.rb
@@ -9,12 +9,29 @@ configure do |c|
   c.nick    = "botspade"
   c.server  = "irc.twitch.tv"
   c.port    = 6667
-  c.password = "" # Get it here: http://twitchapps.com/tmi/
+  c.password = "oauth:cbr74bjxkfc5r24lqs0yph44fgbgam7" # Get it here: http://twitchapps.com/tmi/
   c.verbose = true
 end
 
 on :connect do  # initializations
   join "#watchspade"
+  
+  ############################################################################
+  # Sqllite3 Related setup
+
+  # Lets open up Sqlite3 Database
+  db = SQLite3::Database.new "botspade.db"
+
+  # Initial Tables - points / checkin / viewers / games / bets
+  # We will generate a custom user table so we have a relational ID for other tables.
+  db.execute "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, points INT, first_seen BIGINT, last_seen BIGINT)"
+  db.execute "CREATE UNIQUE INDEX IF NOT EXISTS username ON users (username)"
+
+  # Each checkin will have its own row, With related ID from users table and timestamp of when.
+  db.execute "CREATE TABLE IF NOT EXISTS checkins (id INTEGER PRIMARY KEY, user_id INT, timestamp BIGINT)"
+  # Change win (1) / lose (2) / tie (3) to INTs for database optimisation.
+  db.execute "CREATE TABLE IF NOT EXISTS games (id INTEGER PRIMARY KEY, status TINYINT, timestamp BIGINT)"
+  
   
   # Keeps track of a user's points. DB is persistent. 
   # e.g. {watchspade => 34}
@@ -53,6 +70,9 @@ on :connect do  # initializations
   
   # Toggle whether or not bets are allowed
   @betsopen = FALSE 
+  
+  # Set initial uptime
+  @stream_start_time == "none"
   
   # Whose bot is this?
   @botmaster = "Spade"
