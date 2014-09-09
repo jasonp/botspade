74c46ccd0b4338f51f2a14d3784b13e6b2d5364c
Jason Mountcastle <etheco@gmail.com>
2014-09-10 00:26:46 +0100

    Web App Initialness

--- index.php
+++ index.php
@@ -1,73 +1,46 @@
 <?php
-#
-#  You need to set the correct path for the database file on line 61
-#  for this to work! Place this file in a web-accessible part of your server, 
-#  e.g. /var/www/example.com/public_html/leaderboard/index.php
-#
 
-?>
+$f3=require('app/lib/base.php');
 
-<html>
-<head>
-<title>Points Leaderboard</title>
+$f3->set('AUTOLOAD','app/classes/');
 
-<style type="text/css">
+$f3->set('DEBUG',1);
+if ((float)PCRE_VERSION<7.9)
+	trigger_error('PCRE version is out of date');
 
-body {font-family: Georgia, Times, 'Times New Roman', Serif;}
-h1, h2, h3, h4 {font-family: 'Helvetica Neue', Helvetica, Arial, Sans-Serif;}
-	h4 {padding-left: .5em; margin-bottom: .2em;}
-	
+$f3->config('app/config.ini');
+$f3->config('app/globals.ini');
 
-section {display: block; margin: auto; padding-top: 20px; width: 820px; font-size: 1.2em; line-height: 135%; border-left: 1px dotted #444; border-right: 1px dotted #444; padding-left: 15px;}
-.dotted_header {background: url('dotted-jut.png') repeat-y; padding-left: 50px;}
-	.sched {padding-left: 1em;}
+// Open Database
+$db=new DB\SQL('sqlite:' . $f3->get('DB_FILE'));
+// Lets make sessions use database.
+new Session();
+new \DB\SQL\Session($db);
 
-p {text-align: justify; padding-left: .5em; padding-right: 1em; color: #232323;}
-.notyet {color: #888;}
+if($f3->get('SESSION.user_id')) {
+	$f3->set('user_id', $f3->get('SESSION.user_id'));
+	$f3->set('username', $f3->get('SESSION.twitch_username'));
+}
 
-.subhead {color: #666; margin-bottom: 25px; margin-top: -10px;}
+$f3->route('GET /',
+	function($f3) {
+		// This will be moved to its own class.
+		$db=new DB\SQL('sqlite:botspade.db');
+		$f3->set('results',$db->exec('SELECT u.username, u.points FROM users AS u ORDER BY points DESC'));
 
-.points-header, .username-header {text-decoration: bold; color: #003366; border-bottom: 1px solid #003366; margin-right: 5px; float:left;}
-.points-header {width: 100px;}
-.username-header {width: 600px;}
+		$f3->set('content','welcome.htm');
 
-.user {width: 100%; height: 30px;}
-.points {width: 100px; margin-right: 5px; float: left;}
-.username {display: inline; float: left;}
+		echo View::instance()->render('layout.htm');
+	}
+);
 
-a {color: #999; text-decoration: underline;}
-a:hover {text-decoration: none; color: #bbb;}
+$f3->route('GET /userref',
+	function($f3) {
+		$f3->set('content','userref.htm');
+		echo View::instance()->render('layout.htm');
+	}
+);
 
-	
 
-</style>
 
-</head>
-<body>
-	<section>
-		<?php
-				echo '<h1>Points Leaderboard</h1>';				
-				?> 
-				<div class="subhead">Hit ctrl+f to find your name.</div>
-				
-				<div class="user"> 
-			 		<div class="points-header"> Points </div>
-					<div class="username-header"> Viewer </div>
-				</div> <?php
-
-				#
-				# Set this to the sqlite database file's location
-				#
-				$dir = 'sqlite:/home/jason/botspade/botspade.db';
-				$dbh  = new PDO($dir) or die("cannot open the database");
-				$query =  "SELECT * FROM users ORDER BY points DESC";
-				foreach ($dbh->query($query) as $row)
-				{
-					?> <div class="user"> 
-				 		<div class="points"> <?php echo $row[2]; ?> </div>
-						<div class="username"> <?php echo $row[1]; 
-					?></div> </div> <?php
-				}
-		?>
-	</section>
-</body>
+$f3->run();
